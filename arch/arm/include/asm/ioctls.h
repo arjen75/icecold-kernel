3516c16b4e53e4bbb99a0efe888a6e4bbf77ceac
Randolf Kastenmaier <randolf@kubuntu-server.(none)>
2012-05-23 09:06:45 +0200

    build stuff

--- arch/arm/include/asm/ioctls.h
+++ arch/arm/include/asm/ioctls.h
@@ -2,9 +2,10 @@
 #define __ASM_ARM_IOCTLS_H
 
 #include <asm/ioctl.h>
+#include <asm-generic/ioctls.h>
 
 /* 0x54 is just a magic number to make these relatively unique ('T') */
-
+/*
 #define TCGETS		0x5401
 #define TCSETS		0x5402
 #define TCSETSW		0x5403
@@ -42,21 +43,21 @@
 #define TIOCNOTTY	0x5422
 #define TIOCSETD	0x5423
 #define TIOCGETD	0x5424
-#define TCSBRKP		0x5425	/* Needed for POSIX tcsendbreak() */
-#define TIOCSBRK	0x5427  /* BSD compatibility */
-#define TIOCCBRK	0x5428  /* BSD compatibility */
-#define TIOCGSID	0x5429  /* Return the session ID of FD */
+#define TCSBRKP		0x5425	// Needed for POSIX tcsendbreak()
+#define TIOCSBRK	0x5427  // BSD compatibility
+#define TIOCCBRK	0x5428  // BSD compatibility
+#define TIOCGSID	0x5429  // Return the session ID of FD
 #define TCGETS2		_IOR('T',0x2A, struct termios2)
 #define TCSETS2		_IOW('T',0x2B, struct termios2)
 #define TCSETSW2	_IOW('T',0x2C, struct termios2)
 #define TCSETSF2	_IOW('T',0x2D, struct termios2)
-#define TIOCGPTN	_IOR('T',0x30, unsigned int) /* Get Pty Number (of pty-mux device) */
-#define TIOCSPTLCK	_IOW('T',0x31, int)  /* Lock/unlock Pty */
+#define TIOCGPTN	_IOR('T',0x30, unsigned int) // Get Pty Number (of pty-mux device)
+#define TIOCSPTLCK	_IOW('T',0x31, int)  // Lock/unlock Pty
 
 #define TIOCGRS485      0x542E
 #define TIOCSRS485      0x542F
 
-#define FIONCLEX	0x5450  /* these numbers need to be adjusted. */
+#define FIONCLEX	0x5450  // these numbers need to be adjusted.
 #define FIOCLEX		0x5451
 #define FIOASYNC	0x5452
 #define TIOCSERCONFIG	0x5453
@@ -64,16 +65,16 @@
 #define TIOCSERSWILD	0x5455
 #define TIOCGLCKTRMIOS	0x5456
 #define TIOCSLCKTRMIOS	0x5457
-#define TIOCSERGSTRUCT	0x5458 /* For debugging only */
-#define TIOCSERGETLSR   0x5459 /* Get line status register */
-#define TIOCSERGETMULTI 0x545A /* Get multiport config  */
-#define TIOCSERSETMULTI 0x545B /* Set multiport config */
+#define TIOCSERGSTRUCT	0x5458 // For debugging only
+#define TIOCSERGETLSR   0x5459 // Get line status register
+#define TIOCSERGETMULTI 0x545A // Get multiport config/
+#define TIOCSERSETMULTI 0x545B // Set multiport config
 
-#define TIOCMIWAIT	0x545C	/* wait for a change on serial input line(s) */
-#define TIOCGICOUNT	0x545D	/* read serial port inline interrupt counts */
+#define TIOCMIWAIT	0x545C	// wait for a change on serial input line(s)
+#define TIOCGICOUNT	0x545D	// read serial port inline interrupt counts
 #define FIOQSIZE	0x545E
 
-/* Used for packet mode */
+// Used for packet mode 
 #define TIOCPKT_DATA		 0
 #define TIOCPKT_FLUSHREAD	 1
 #define TIOCPKT_FLUSHWRITE	 2
@@ -82,6 +83,6 @@
 #define TIOCPKT_NOSTOP		16
 #define TIOCPKT_DOSTOP		32
 
-#define TIOCSER_TEMT	0x01	/* Transmitter physically empty */
-
+#define TIOCSER_TEMT	0x01	// Transmitter physically empty
+*/
 #endif
